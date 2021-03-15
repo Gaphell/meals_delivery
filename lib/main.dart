0b5c71c85bb92015d4e4a3fd8c4677b066af2a4e
Tenzin Gaphel <tenzin606gaphel@gmail.com>
2021-03-15 14:45:28 +0600

    meals details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:meals_delivery/views/categories_meals_screen.dart';
 import 'package:meals_delivery/views/categories_screen.dart';
+import 'package:meals_delivery/views/meal_detail_screen.dart';
+
+import 'views/tabs_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,10 +16,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.greenAccent
-      ),
-      home: CategoriesScreen(),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(235, 255, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                bodyText2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                headline6: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotCondensed',
+                ),
+              )),
+      // home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => TabsScreen(),
+        CategoryMealScreen.routeNames: (context) => CategoryMealScreen(),
+        MealDetailScreen.routeNames: (context) => MealDetailScreen(),
+      },
+      // onGenerateRoute: (settings) {
+      //   return MaterialPageRoute(
+      //     builder: (ctx) => CategoriesScreen(),
+      //   );
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
     );
   }
 }
---
+++ lib/views/tabs_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:meals_delivery/widgets/main_drawer.dart';
+import 'categories_screen.dart';
+import 'favorites.screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, Object>> pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoritesScreen(), 'title': 'Favorites'},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          )
+        ],
+      ),
+    );
+  }
+}
